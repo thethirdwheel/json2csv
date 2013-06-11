14ee60402c0121f172f7b62895f0079c4ec19341
Rory Carmichael <rcarmichael@quantcast.com>
2013-06-10 21:20:53 -0700

    make test case pass for json2csv_test.go

--- json2csv_test.go
+++ json2csv_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestKeys(t *testing.T) {
-	const in, out = 2, 5
+	const in, out = 2, 4
 	if x := out - in; x != in {
 		t.Errorf("%v - %v = %v; want %v", out, in, x, in)
 	}
